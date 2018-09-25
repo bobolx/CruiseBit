08ab184eb8ada64c40d16dd3dffec8307cb31bb5
lixiang <214372150@qq.com>
2018-09-25 15:17:37 +0800

    test time

--- main.ts
+++ main.ts
@@ -7,19 +7,19 @@ namespace CruiseBit {
     /**
      * 设置电机
      */
-    //% blockId="cruise_motor" block="电机 左 速度%leftSpeed| 右 速度%rightSpeed"
+    //% blockId="cruise_motor" block="电机 左 速度%leftSpeed| 右 速度%rightSpeed| 时长%time 秒"
     //% leftSpeed.min=-1023 leftSpeed.max=1023
     //% rightSpeed.min=-1023 rightSpeed.max=1023
     //% weight=100
-    export function motorRun(leftSpeed: number, rightSpeed: number): void {
-        let leftRotation = 0;
+    export function motorRun(leftSpeed: number, rightSpeed: number, time: number): void {
+        let leftRotation = 1;
         if(leftSpeed < 0){
-            leftRotation = 1;
+            leftRotation = 0;
         }
 
-        let rightRotation = 0;
+        let rightRotation = 1;
         if(rightSpeed < 0){
-            rightRotation = 1;
+            rightRotation = 0;
         }
         
        //左电机 M1
@@ -29,6 +29,17 @@ namespace CruiseBit {
         //右电机 M2
         pins.analogWritePin(AnalogPin.P16, Math.abs(rightSpeed));
         pins.digitalWritePin(DigitalPin.P15, rightRotation);
+
+        //添加时间控制
+        setTimeout(function(){
+            //左电机 M1
+            pins.analogWritePin(AnalogPin.P14, 0);
+            pins.digitalWritePin(DigitalPin.P13, 0);
+            //右电机 M2
+            pins.analogWritePin(AnalogPin.P16, 0);
+            pins.digitalWritePin(DigitalPin.P15, 0);
+        },time*1000);
+        
         
     }
 
