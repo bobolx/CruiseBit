48137e1e40d8b1fa9a643f8a40cbc6b5edddf82c
lixiang <214372150@qq.com>
2018-09-26 18:13:11 +0800

    test

--- main.ts
+++ main.ts
@@ -103,13 +103,13 @@ namespace CruiseBit {
     let neoStrip = neopixel.create(DigitalPin.P1, 9, NeoPixelMode.RGB);
 
     /**
-     * 设置电机
+     * 设置电机有时长限制
      */
-    //% blockId="cruise_motor" block="电机 左 速度%leftSpeed| 右 速度%rightSpeed| 时长%time 秒"
+    //% blockId="cruise_motor_time" block="电机 左 速度%leftSpeed| 右 速度%rightSpeed| 时长%time 秒"
     //% leftSpeed.min=-1023 leftSpeed.max=1023
     //% rightSpeed.min=-1023 rightSpeed.max=1023
     //% weight=100
-    export function motorRun(leftSpeed: number, rightSpeed: number, time: number): void {
+    export function motorRunTime(leftSpeed: number, rightSpeed: number, time: number): void {
         let leftRotation = 1;
         if(leftSpeed < 0){
             leftRotation = 0;
@@ -144,6 +144,34 @@ namespace CruiseBit {
         pins.analogWritePin(AnalogPin.P16, 0);
         pins.digitalWritePin(DigitalPin.P15, 0);
         
+        
+    }
+
+    /**
+     * 设置电机
+     */
+    //% blockId="cruise_motor" block="电机 左 速度%leftSpeed| 右 速度%rightSpeed"
+    //% leftSpeed.min=-1023 leftSpeed.max=1023
+    //% rightSpeed.min=-1023 rightSpeed.max=1023
+    //% weight=100
+    export function motorRun(leftSpeed: number, rightSpeed: number): void {
+        let leftRotation = 1;
+        if(leftSpeed < 0){
+            leftRotation = 0;
+        }
+
+        let rightRotation = 1;
+        if(rightSpeed < 0){
+            rightRotation = 0;
+        }
+        
+       //左电机 M1
+        pins.analogWritePin(AnalogPin.P14, Math.abs(leftSpeed));
+        pins.digitalWritePin(DigitalPin.P13, leftRotation);
+
+        //右电机 M2
+        pins.analogWritePin(AnalogPin.P16, Math.abs(rightSpeed));
+        pins.digitalWritePin(DigitalPin.P15, rightRotation);
         
     }
 
